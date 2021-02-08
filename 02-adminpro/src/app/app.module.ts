c8c590cb59b31eb7461114a3c01f9d9ecb68e64d
Mauro Gauto <mauro.gauto@globallogic.com>
2021-02-08 17:05:09 -0300

    remove conflicts

--- 02-adminpro/src/app/app.module.ts
+++ 02-adminpro/src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { LoginComponent } from './auth/login/login.component';
 import { RegisterComponent } from './auth/register/register.component';
 import { NopagefoundComponent } from './pages/nopagefound/nopagefound.component';
@@ -10,15 +9,10 @@ import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 import { SidebarComponent } from './shared/sidebar/sidebar.component';
 import { HeaderComponent } from './shared/header/header.component';
-=======
-import { HomeComponent } from './pages/home/home.component';
-import { AboutComponent } from './pages/about/about.component';
->>>>>>> 0ca6f121fa01f31c7b15e6f34c39f1a2be8acc1f
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
     LoginComponent,
     RegisterComponent,
     NopagefoundComponent,
@@ -26,10 +20,6 @@ import { AboutComponent } from './pages/about/about.component';
     BreadcrumbsComponent,
     SidebarComponent,
     HeaderComponent
-=======
-    HomeComponent,
-    AboutComponent
->>>>>>> 0ca6f121fa01f31c7b15e6f34c39f1a2be8acc1f
   ],
   imports: [
     BrowserModule
