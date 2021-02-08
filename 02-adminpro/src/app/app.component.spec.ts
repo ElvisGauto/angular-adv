2dcc8bc2fe447282a5af0e4606de18b5e0f14171
Mauro Gauto <mauro.gauto@globallogic.com>
2021-02-08 17:04:09 -0300

    remove clitest from adminpro

--- 02-adminpro/src/app/app.component.spec.ts
+++ 02-adminpro/src/app/app.component.spec.ts
@@ -16,27 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-<<<<<<< HEAD
   it(`should have as title 'adminpro'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('adminpro');
-=======
-  it(`should have as title 'clitest'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('clitest');
->>>>>>> 0ca6f121fa01f31c7b15e6f34c39f1a2be8acc1f
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-<<<<<<< HEAD
     expect(compiled.querySelector('.content span').textContent).toContain('adminpro app is running!');
-=======
-    expect(compiled.querySelector('.content span').textContent).toContain('clitest app is running!');
->>>>>>> 0ca6f121fa01f31c7b15e6f34c39f1a2be8acc1f
   });
 });
